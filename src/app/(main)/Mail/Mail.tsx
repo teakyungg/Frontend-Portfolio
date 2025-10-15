385a35d1ff56967da1df1868803d1a962df5c663
teakyungg <kak0502@naver.com>
2025-10-15 22:38:39 +0900

    feat/aside aside

---
+++ src/app/(main)/Mail/Mail.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useRef } from "react";
+import styles from "./Mail.module.scss";
+
+/* 메일 클립 보드에 복사하는 기능 넣기 */
+
+export function Mail() {
+  const mailAddress = useRef<null | HTMLDivElement>(null);
+
+  const copyAddress = async () => {
+    if (mailAddress.current) {
+      try {
+        await navigator.clipboard.writeText("rlaxorud8532@gmail.com");
+        alert("클립 보드에 복사되었습니다!");
+      } catch {
+        alert("복사에 실패하였습니다. 다시 시도해 주세요");
+      }
+    }
+  };
+
+  return (
+    <div className={styles.mailInner}>
+      {/* 메일 바로 보내기 */}
+      <div className={styles.list}>
+        <a
+          className={`${styles.mailLink} ${styles.mailLinkImage}`}
+          href="mailto:rlaxorud8532@gmail.com"
+          target="_blank"
+        ></a>
+        <span className={styles.title}>메일 바로 보내기</span>
+      </div>
+
+      {/* 메일 클립 보드 복사하기 */}
+      <div className={styles.list}>
+        <div className={`${styles.mailLink} ${styles.mailImage}`} ref={mailAddress} onClick={copyAddress}></div>
+        <span className={styles.title}>메일 주소 복사하기</span>
+      </div>
+    </div>
+  );
+}
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,6 @@
 import { AboutMe } from "./AboutMe/AboutMe";
 import { Intro } from "./IntroContent/IntroContent";
+import { Mail } from "./Mail/Mail";
 import { Project } from "./Project/Project";
 import { Skills } from "./Skills/Skills";
 
@@ -27,7 +28,14 @@ export default function Main() {
         <AboutMe />
       </section>
 
-      {/* 마지막에 읽어 주셔서 감사합니다 쓰는 것 좋을듯 */}
+      {/* 메일 보내는 부분 */}
+      <aside>
+        <Mail />
+      </aside>
+
+      {/* 자기소개 부분에 메일 클라이언트 추가하기 */}
+      {/* 맨 밑에 읽어주셔서 감사합니다라는 거 넣기 */}
+      {/* 그 다음 각 프로젝트 소개하는 페이지 만들고 이력서 써서 넣으면 될듯 */}
     </>
   );
 }
