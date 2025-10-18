c607ad3fb4614e9e620c5e30f325706c7948d6a9
teakyungg <kak0502@naver.com>
2025-10-18 19:12:42 +0900

    feat/starbucksModal create

--- src/app/(main)/Project/ProjectModal/Starbucks/StarbucksModal.tsx
+++ src/app/(main)/Project/ProjectModal/Starbucks/StarbucksModal.tsx
@@ -6,22 +6,28 @@ interface Starbucks {
   setModal: () => void;
 }
 
-/* 이거 모달 공통된 부분 공용 컴포넌트로 분리하기 */
-
 export function StarbucksModal({ setModal }: Starbucks) {
   return (
     <ProjectModal closeFn={setModal}>
       <div className={styles.modalInner}>
-        {/* karly 이미지 */}
         <div className={styles.imageInner}>
-          <a href="https://vision-karly.netlify.app/src/item/main" target="blank" className={styles.linkInner}>
+          <a href="https://startbycks-teaktung.netlify.app/" target="blank" className={styles.linkInner}>
             <span className={styles.linkButton}>바로가기</span>
           </a>
         </div>
 
         {/* 내용 */}
         <div className={styles.textInner}>
-          <p>스타벅스</p>
+          <p>
+            <TextStrong>Vite</TextStrong>를 활용하여 빠른 개발 환경 구축 및 웹 페이지 빌드 테스트를 진행하였습니다.
+          </p>
+          <p>
+            <TextStrong>GSAP 라이브러리</TextStrong>를 사용해 스크롤 애니메이션을 효율적으로 구현하였습니다.
+          </p>
+          <p>
+            <TextStrong>Swiper 라이브러리</TextStrong>를 활용해 캐러셀 기능을 빠르고 직관적으로 구현하였습니다.
+          </p>
+          <p>CSS 리셋을 적용하여 브라우저 간 스타일 일관성을 유지하였습니다.</p>
         </div>
       </div>
     </ProjectModal>
