1204efc94d9a158ec78e269017ab478f21fb5d15
teakyungg <kak0502@naver.com>
2025-11-04 17:22:24 +0900

    feat/studyLog add notionLink "Browser operation flow"

--- src/app/(main)/StudyLog/StudyLog.tsx
+++ src/app/(main)/StudyLog/StudyLog.tsx
@@ -11,10 +11,16 @@ export function StudyLog() {
         <span className={styles.titleExplanation}>학습 과정과 실험 내용을 정리한 Notion 페이지입니다.</span>
 
         <div className={styles.NotionLinkBox}>
-          {/* Browser의 동작 속성 정리 페이지 */}
+          {/* Browser의 동작 */}
           <NotionLink
             title="Browser의 동작"
             link="https://florentine-soy-ff3.notion.site/Browser-262abb3b12b6805fa812d710f4ca1082?source=copy_link"
+          />
+
+          {/* Browser의 동작 흐름 */}
+          <NotionLink
+            title="Browser의 동작 흐름"
+            link="https://florentine-soy-ff3.notion.site/Browser-262abb3b12b680d8a1d7c97e660499c8?source=copy_link"
           />
 
           {/* float 속성 정리 페이지 */}
