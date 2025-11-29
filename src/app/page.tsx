d0928f7aa16232aba0e1d001f43a7c8bac55e8a6
teakyungg <kak0502@naver.com>
2025-11-29 22:12:44 +0900

    refactoring/organizing the whole structure of the intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { AboutMe } from "./AboutMe/AboutMe";
 import { Ending } from "./Ending/Ending";
-import { Intro } from "./IntroContent/IntroContent";
+import { Intro } from "./IntroContent/Intro";
 import { Mail } from "./Mail/Mail";
 import { Project } from "./Project/Project";
 import { Skills } from "./Skills/Skills";
