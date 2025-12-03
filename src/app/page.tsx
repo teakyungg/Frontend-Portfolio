c2742c2a81973057a6c7e9c32c61978c7cb1b53b
teakyungg <kak0502@naver.com>
2025-12-03 16:54:05 +0900

    refactoring/remove spaces main page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,28 +9,13 @@ import { StudyLog } from "./StudyLog/StudyLog";
 export default function Main() {
   return (
     <>
-      {/* 인트로 */}
       <Intro />
-
-      {/* project */}
       <Project />
-
-      {/* Skills */}
       <Skills />
-
-      {/* 개념 정리 */}
       <StudyLog />
-
-      {/* about 자기소개 */}
       <AboutMe />
-
-      {/* ending */}
       <Ending />
-
-      {/* 메일 보내는 부분 */}
       <Mail />
-
-      {/* 각 프로젝트 소개하는 페이지 만들고 이력서 써서 넣으면 될듯 */}
     </>
   );
 }
