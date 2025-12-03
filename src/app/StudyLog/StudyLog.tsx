11bc7a5320915863f510640acd051fe2be9812cd
teakyungg <kak0502@naver.com>
2025-12-03 23:36:37 +0900

    feat/add studyLog.tsx "z-index"

--- src/app/StudyLog/StudyLog.tsx
+++ src/app/StudyLog/StudyLog.tsx
@@ -11,8 +11,9 @@ export function StudyLog() {
     {title: "Browser의 동작 흐름", link: "https://florentine-soy-ff3.notion.site/Browser-262abb3b12b680d8a1d7c97e660499c8?source=copy_link"},
     {title: "float", link: "https://florentine-soy-ff3.notion.site/float-297abb3b12b680bea5d3fe991fa3d6ec?source=copy_link"},
     {title: "position", link: "https://florentine-soy-ff3.notion.site/position-29babb3b12b6805e8bd2f74165803ffb"},
+    {title: "z-index", link: "https://florentine-soy-ff3.notion.site/z-index-2bbabb3b12b6808eaa40f099a66acd32?pvs=73"},
+    {title: "box-sizing", link: "https://florentine-soy-ff3.notion.site/box-sizing-2beabb3b12b6800fa2cfd1225d682023"},
     {title: "React", link: "https://florentine-soy-ff3.notion.site/React-2a1abb3b12b68016a562f274b8491b89?source=copy_link"},
-    {title: "Z-index", link: "https://florentine-soy-ff3.notion.site/z-index-2bbabb3b12b6808eaa40f099a66acd32?pvs=73"},
   ];
 
   const linkEl = linkData.map(value => <NotionLink key={value.title} title={value.title} link={value.link}/>)
