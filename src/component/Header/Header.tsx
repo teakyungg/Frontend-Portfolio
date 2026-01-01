9880620029e715db9923912c34a4fbf06f91bba6
teakyungg <kak0502@naver.com>
2026-01-01 17:32:21 +0900

    feat/Header add responsive

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -2,20 +2,23 @@
 import Link from "next/link";
 import styles from "./Header.module.css";
 
+import gsap from "gsap";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+gsap.registerPlugin(ScrollToPlugin);
+
 export default function Header() {
   const linkName = ["Project", "Skill", "StudyLog", "About"]; // 링크 제목
-  const scrollLoction = [850, 1500, 2000, 2520]; // 버튼 클릭 시 스크롤 포지션
 
-  const scrollToCustomPosition = (i: number) => {
-    window.scrollTo({
-      top: scrollLoction[i],
-      behavior: "smooth", // 부드럽게 이동
+  const scrollToTarget = (id: string) => {
+    const headerEl = document.querySelector("header"); // 또는 너의 헤더 클래스 선택자
+    const headerH = headerEl ? headerEl.getBoundingClientRect().height : 0;
+
+    gsap.to(window, {
+      duration: 0.8,
+      scrollTo: { y: `#${id}`, offsetY: headerH, autoKill: true },
+      ease: "power2.out",
     });
   };
-
-  const linkItem = linkName.map((i, index) => (
-    <li key={i} onClick={() => {scrollToCustomPosition(index);}}>{i}</li>
-  ));
 
   return (
     <header className={styles.header}>
@@ -23,7 +26,18 @@ export default function Header() {
         <Link href={"/"} className={styles.title}>
           PORTFOLIO
         </Link>
-        <ul className={styles.list}>{linkItem}</ul>
+        <ul className={styles.list}>
+          {linkName.map((i) => (
+            <li
+              key={i}
+              onClick={() => {
+                scrollToTarget(i);
+              }}
+            >
+              {i}
+            </li>
+          ))}
+        </ul>
       </div>
     </header>
   );
